501fc97245ed8b34662ab552d8e8dc10458ae460
DreMind5000 <89564302+DreMind5000@users.noreply.github.com>
2021-10-17 12:37:31 -0400

    Updates

---
+++ precache-manifest.28c57738c2e5da4f54c988560b189ff3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc099d92c13940867661f66d6c5d596e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a4520cb086a2427e4580",
+    "url": "./static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "5a6df1d6187ee9e98d55",
+    "url": "./static/css/main.31702a03.chunk.css"
+  },
+  {
+    "revision": "a4520cb086a2427e4580",
+    "url": "./static/js/2.1de0c018.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "./static/js/2.1de0c018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a6df1d6187ee9e98d55",
+    "url": "./static/js/main.b9f06b1c.chunk.js"
+  },
+  {
+    "revision": "e720711dd3e4e8b209d0",
+    "url": "./static/js/runtime-main.43c7aded.js"
+  },
+  {
+    "revision": "2565884a03843bf5f37bf4a806402acf",
+    "url": "./static/media/avalanche-avax-logo.2565884a.png"
+  },
+  {
+    "revision": "421d18cc3447587c7d16480ed61ecd14",
+    "url": "./static/media/bnb-coin.421d18cc.svg"
+  },
+  {
+    "revision": "50ef9f2c3d2ab7649be46cb917b2a74f",
+    "url": "./static/media/file-upload-hero.50ef9f2c.svg"
+  },
+  {
+    "revision": "2410baa7a2e708e2952f53672b677683",
+    "url": "./static/media/logo.2410baa7.png"
+  },
+  {
+    "revision": "561bc3f544a9969a74ebb250756667fe",
+    "url": "./static/media/matic-coin.561bc3f5.svg"
+  },
+  {
+    "revision": "e58771e482a12921c5a13566c2bd892f",
+    "url": "./static/media/pdf-ipfs.e58771e4.png"
+  }
+]);
